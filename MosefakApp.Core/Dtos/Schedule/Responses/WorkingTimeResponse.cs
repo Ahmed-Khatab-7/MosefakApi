989bed78a4a06b6f2b545b34dd09c51aa326b6db
Ahmed Khatab <Ahmedkhatab11@outlook.com>
2025-04-23 20:16:28 +0200

    Update Working Time Respose

--- MosefakApp.Core/Dtos/Schedule/Responses/WorkingTimeResponse.cs
+++ MosefakApp.Core/Dtos/Schedule/Responses/WorkingTimeResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace MosefakApp.Core.Dtos.Schedule.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace MosefakApp.Core.Dtos.Schedule.Responses
 {
     public class WorkingTimeResponse
     {
         public string Id { get; set; } = null!;
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public DayOfWeek Day { get; set; }
         public List<PeriodResponse> Periods { get; set; } = new List<PeriodResponse>();
         public bool IsAvailable { get; set; }
